8d8d10a8c29c41d9873a577f775b60b36b9c4af8
frodrok <fredrik@c-1c57e353.048-70-73746f29.cust.bredbandsbolaget.se>
2015-09-01 11:15:29 +0200

    Bash-generated commit message

--- src/main/java/TicTacToeClient.java
+++ src/main/java/TicTacToeClient.java
@@ -12,7 +12,10 @@ public class TicTacToeClient {
             PrintWriter out = new PrintWriter(socket.getOutputStream());
             BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 
-            System.out.println(in.readLine());
+            String line;
+            while ((line = in.readLine()) != null) {
+                System.out.println(line);
+            }
 
             out.write("wow");
 
